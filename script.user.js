c91e74760fd2b8cd6135988ae394766eb4956d5b
SYE <mohamedyounis.job@gmail.com>
2026-01-28 14:26:33 +0200

    Update user script for Tamkeen Pro Ultra

--- script.user.js
+++ script.user.js
@@ -1,13 +1,10 @@
 // ==UserScript==
-// @name         Tamkeen Pro Ultra
+// @name         Tamkeen Pro Ultra Max
 // @namespace    http://tampermonkey.net/
-// @version      2.0
-// @description  تشغيل تلقائي على جميع صفحات تمكين الفرعية
+// @version      3.0
+// @description  تعبئة الملاحظات تلقائياً - نسخة مطورة جداً
 // @author       MOYOUNIS
 // @match        https://sye.tamkeenapp.com/*
-// @match        http://sye.tamkeenapp.com/*
-// @match        https://sye.tamkeenapp.com/finances/transactions/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tamkeenapp.com
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
@@ -15,13 +12,13 @@
 (function() {
     'use strict';
 
-    let selectedPaymentNote = "";
+    let selectedNote = "";
 
-    function autoFillTamkeen() {
-        // البحث عن القائمة
-        const paymentSelect = document.querySelector('select[ng-model="transaction.info.method"]');
+    function doMagic() {
+        // 1. البحث عن قائمة الدفع
+        const pSelect = document.querySelector('select[ng-model*="method"]');
         
-        if (paymentSelect && !paymentSelect.dataset.modified) {
+        if (pSelect && !pSelect.dataset.done) {
             const options = [
                 { label: "Cash", val: "string:cache", note: "Cash Payment" },
                 { label: "Credit card", val: "string:credit-card", note: "Credit card Payment" },
@@ -35,41 +32,56 @@
                 { label: "Client to Client Transfer", val: "string:balance", note: "Method: Client to Client Transfer" }
             ];
 
-            paymentSelect.innerHTML = '';
+            pSelect.innerHTML = '';
             options.forEach(opt => {
                 let o = document.createElement('option');
                 o.text = opt.label;
                 o.value = opt.val;
-                o.setAttribute('data-note', opt.note);
-                paymentSelect.add(o);
+                o.setAttribute('data-msg', opt.note);
+                pSelect.add(o);
             });
 
-            paymentSelect.addEventListener('change', function() {
-                const selected = paymentSelect.options[paymentSelect.selectedIndex];
-                selectedPaymentNote = selected.getAttribute('data-note') || "";
-                injectNow();
+            pSelect.addEventListener('change', () => {
+                const sel = pSelect.options[pSelect.selectedIndex];
+                selectedNote = sel.getAttribute('data-msg') || "";
+                fillNow();
             });
-
-            paymentSelect.dataset.modified = "true";
+            pSelect.dataset.done = "true";
         }
-        injectNow();
+        fillNow();
     }
 
-    function injectNow() {
-        // جربنا هنا الاحتمالين (note و notes) احتياطياً
-        const notesBox = document.querySelector('textarea[ng-model="transaction.info.note"]') || 
-                         document.querySelector('textarea[ng-model="transaction.info.notes"]');
+    function fillNow() {
+        if (!selectedNote) return;
 
-        if (notesBox && selectedPaymentNote !== "") {
-            if (notesBox.value !== selectedPaymentNote) {
-                notesBox.value = selectedPaymentNote;
-                notesBox.dispatchEvent(new Event('input', { bubbles: true }));
-                notesBox.dispatchEvent(new Event('change', { bubbles: true }));
-                notesBox.dispatchEvent(new Event('blur', { bubbles: true }));
+        // بنجرب كل الـ Selectors الممكنة لخانة الملاحظات
+        const box = document.querySelector('textarea[ng-model*="note"]') || 
+                    document.querySelector('textarea[name*="note"]') || 
+                    document.querySelector('.form-control[ng-model*="transaction"]');
+
+        if (box) {
+            if (box.value !== selectedNote) {
+                box.value = selectedNote;
+                
+                // أهم جزء: إجبار السيستم إنه يحس بالتغيير
+                const events = ['input', 'change', 'blur'];
+                events.forEach(e => {
+                    box.dispatchEvent(new Event(e, { bubbles: true }));
+                });
+
+                // تحديث يدوي للـ Angular Scope لو أمكن
+                if (window.angular) {
+                    const element = window.angular.element(box);
+                    const scope = element.scope();
+                    if (scope) {
+                        scope.$apply(() => {
+                            scope.transaction.info.note = selectedNote;
+                        });
+                    }
+                }
             }
         }
     }
 
-    // فحص سريع جداً كل 200 ملي ثانية عشان يلحق التغييرات في اللينك
-    setInterval(autoFillTamkeen, 200);
+    setInterval(doMagic, 300);
 })();
