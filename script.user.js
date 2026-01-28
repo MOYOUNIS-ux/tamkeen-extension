f414d32919434c70381b42b106eb4321bd59f3d3
SYE <mohamedyounis.job@gmail.com>
2026-01-28 14:32:53 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,87 +1,48 @@
 // ==UserScript==
 // @name         Tamkeen Pro Ultra Max
 // @namespace    http://tampermonkey.net/
-// @version      3.0
-// @description  تعبئة الملاحظات تلقائياً - نسخة مطورة جداً
+// @version      3.5
+// @description  إصلاح شامل واختيارات دفع جديدة
 // @author       MOYOUNIS
 // @match        https://sye.tamkeenapp.com/*
 // @grant        none
-// @run-at       document-idle
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    let selectedNote = "";
+    const options = [
+        { label: "Cash", val: "string:cache", note: "Cash Payment" },
+        { label: "Credit card", val: "string:credit-card", note: "Credit card Payment" },
+        { label: "InstaPay", val: "string:bank", note: "Method: InstaPay" },
+        { label: "VC-01001883884", val: "string:cheque", note: "Wallet: 01001883884" },
+        { label: "VC-01022850171", val: "string:cheque", note: "Wallet: 01022850171" },
+        { label: "VC-01064223325", val: "string:cheque", note: "Wallet: 01064223325" },
+        { label: "VC-01068416733", val: "string:cheque", note: "Wallet: 01068416733" },
+        { label: "EC-01111327387", val: "string:cheque", note: "Wallet: 01111327387" },
+        { label: "Client Balance", val: "string:balance", note: "Method: Client Balance" },
+        { label: "Client to Client Transfer", val: "string:balance", note: "Method: Client to Client Transfer" }
+    ];
 
-    function doMagic() {
-        // 1. البحث عن قائمة الدفع
-        const pSelect = document.querySelector('select[ng-model*="method"]');
-        
-        if (pSelect && !pSelect.dataset.done) {
-            const options = [
-                { label: "Cash", val: "string:cache", note: "Cash Payment" },
-                { label: "Credit card", val: "string:credit-card", note: "Credit card Payment" },
-                { label: "InstaPay", val: "string:bank", note: "Method: InstaPay" },
-                { label: "VC-01001883884", val: "string:cheque", note: "Wallet: 01001883884" },
-                { label: "VC-01022850171", val: "string:cheque", note: "Wallet: 01022850171" },
-                { label: "VC-01064223325", val: "string:cheque", note: "Wallet: 01064223325" },
-                { label: "VC-01068416733", val: "string:cheque", note: "Wallet: 01068416733" },
-                { label: "EC-01111327387", val: "string:cheque", note: "Wallet: 01111327387" },
-                { label: "Client Balance", val: "string:balance", note: "Method: Client Balance" },
-                { label: "Client to Client Transfer", val: "string:balance", note: "Method: Client to Client Transfer" }
-            ];
+    function inject() {
+        const select = document.querySelector('select[ng-model*="method"]');
+        const noteBox = document.querySelector('textarea[ng-model*="note"]');
 
-            pSelect.innerHTML = '';
-            options.forEach(opt => {
-                let o = document.createElement('option');
-                o.text = opt.label;
-                o.value = opt.val;
-                o.setAttribute('data-msg', opt.note);
-                pSelect.add(o);
-            });
-
-            pSelect.addEventListener('change', () => {
-                const sel = pSelect.options[pSelect.selectedIndex];
-                selectedNote = sel.getAttribute('data-msg') || "";
-                fillNow();
-            });
-            pSelect.dataset.done = "true";
-        }
-        fillNow();
-    }
-
-    function fillNow() {
-        if (!selectedNote) return;
-
-        // بنجرب كل الـ Selectors الممكنة لخانة الملاحظات
-        const box = document.querySelector('textarea[ng-model*="note"]') || 
-                    document.querySelector('textarea[name*="note"]') || 
-                    document.querySelector('.form-control[ng-model*="transaction"]');
-
-        if (box) {
-            if (box.value !== selectedNote) {
-                box.value = selectedNote;
-                
-                // أهم جزء: إجبار السيستم إنه يحس بالتغيير
-                const events = ['input', 'change', 'blur'];
-                events.forEach(e => {
-                    box.dispatchEvent(new Event(e, { bubbles: true }));
-                });
-
-                // تحديث يدوي للـ Angular Scope لو أمكن
-                if (window.angular) {
-                    const element = window.angular.element(box);
-                    const scope = element.scope();
-                    if (scope) {
-                        scope.$apply(() => {
-                            scope.transaction.info.note = selectedNote;
-                        });
-                    }
+        if (select && !select.dataset.loaded) {
+            select.innerHTML = options.map(opt => `<option value="${opt.val}" data-note="${opt.note}">${opt.label}</option>`).join('');
+            select.addEventListener('change', () => {
+                const note = select.options[select.selectedIndex].getAttribute('data-note');
+                if (noteBox) {
+                    noteBox.value = note;
+                    noteBox.dispatchEvent(new Event('input', { bubbles: true }));
+                    noteBox.dispatchEvent(new Event('change', { bubbles: true }));
                 }
-            }
+            });
+            select.dataset.loaded = "true";
         }
     }
 
-    setInterval(doMagic, 300);
+    // تكرار الفحص لضمان الظهور حتى لو الصفحة بطيئة
+    setInterval(inject, 500);
 })();
